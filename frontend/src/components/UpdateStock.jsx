58dde0c5f2c49f2149b708153b75334008e3bea8
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2023-11-25 22:30:27 +0530

    Update UpdateStock.jsx

--- frontend/src/components/UpdateStock.jsx
+++ frontend/src/components/UpdateStock.jsx
@@ -1,6 +1,6 @@
 // UpdateStock.jsx
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { updateStock, searchStockById } from "../service/service";
 import useAuth from "../service/useAuth";
 
