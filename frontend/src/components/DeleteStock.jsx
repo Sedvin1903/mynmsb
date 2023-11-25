023a00eb30276e115d9b7627e58583953d4be978
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2023-11-25 22:27:32 +0530

    Update DeleteStock.jsx

--- frontend/src/components/DeleteStock.jsx
+++ frontend/src/components/DeleteStock.jsx
@@ -1,6 +1,6 @@
 // DeleteStock.jsx
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { deleteStock, searchStockById } from "../service/service";
 import useAuth from "../service/useAuth";
 
