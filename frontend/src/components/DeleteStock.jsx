aacb546e7f09c5e8738207aba0bea4cbcbb0d952
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2023-11-26 21:25:38 +0530

    Update DeleteStock.jsx

--- frontend/src/components/DeleteStock.jsx
+++ frontend/src/components/DeleteStock.jsx
@@ -1,12 +1,12 @@
 // DeleteStock.jsx
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { deleteStock, searchStockById } from "../service/service";
+import { searchStockById,deleteStock } from "../service/service";
 import useAuth from "../service/useAuth";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const DeleteStock = () => {
   const [stockId, setStockId] = useState("");
+  //const navigate = useNavigate();
   const { requireAuth } = useAuth();
 
   // Check if the user is authenticated before rendering the component
@@ -14,20 +14,21 @@ const DeleteStock = () => {
     requireAuth();
   }, [requireAuth]);
 
-   const deleteStockData = async () => {
+  async function deleteStockData() {
     try {
       const response1 = await searchStockById(stockId);
-     // console.log(response1);
+      console.log(response1);
       if (response1 === "null") {
         alert("Stock Not Found!");
       } 
       else {
-        const response2 = await deleteStock(stockId);
-          if (response2 !== "null") 
-          { alert("Stock deleted successfully!");}
+      await deleteStock(stockId);
+  alert("Stock deleted successfully!");
       }
+
       resetForm();
-    } catch (err) {
+    } 
+    catch (err) {
       console.error("Error deleting stock:", err);
       alert("Error deleting stock: " + err.message);
     }
@@ -41,41 +42,26 @@ const DeleteStock = () => {
     <div>
       {/* Your navigation bar */}
       <nav
-        className="navbar navbar-expand-lg navbar-light"
+        className="navbar navbar-light"
         style={{ backgroundColor: "#485c7f" }}
       >
         <div className="container-fluid">
           <Link to="/home" className="navbar-brand text-white">
             StockEasy
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <div className="navbar-nav ms-auto">
-              {" "}
-              {/* "ms-auto" pushes the links to the right */}
-              <Link to="/home" className="nav-link text-white">
-                Home
-              </Link>
-              <Link to="/add" className="nav-link text-white">
-                Add Stocks
-              </Link>
-              <Link to="/update" className="nav-link text-white">
-                Update Stocks
-              </Link>
-              <Link to="/" className="btn btn-light rounded-pill">
-                Logout
-              </Link>
-            </div>
+          <div className="d-flex align-items-center">
+            <Link to="/home" className="nav-link me-3 text-white">
+              Home
+            </Link>
+            <Link to="/add" className="nav-link me-3 text-white">
+              Add Stocks
+            </Link>
+            <Link to="/update" className="nav-link me-3 text-white">
+              Update Stocks
+            </Link>
+            <Link to="/" className="btn btn-light me-3 rounded-pill">
+              Logout
+            </Link>
           </div>
         </div>
       </nav>
@@ -93,14 +79,13 @@ const DeleteStock = () => {
             />
           </div>
 
-          <div className="d-flex justify-content-center">
-            <button
-              className="btn btn-danger col-12 col-md-2"
-              onClick={deleteStockData}
-            >
-              Delete Stock
-            </button>
-          </div>
+          <button
+            type="button"
+            className="btn btn-danger col-2"
+            onClick={deleteStockData}
+          >
+            Delete Stock
+          </button>
         </form>
       </div>
     </div>
